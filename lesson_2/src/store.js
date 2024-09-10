377cd16ea2d0ccbf4fb6f88edcb9f7d4879f7722
dsadasffdsf <qwertfdsazxcvg122@mail.ru>
2024-09-10 18:51:50 +0300

    lesson_2

---
+++ lesson_2/src/store.js
@@ -0,0 +1,98 @@
+/**
+ * Хранилище состояния приложения
+ */
+class Store {
+  constructor(initState = {}) {
+    const updatedList = [
+      ...initState.list.map(item => {
+        return { ...item, selection: 0, selected: false };
+      }),
+    ];
+
+    this.state = {
+      list: [...updatedList],
+      listLength: initState.list.length + 1,
+    };
+    this.listeners = []; // Слушатели изменений состояния
+  }
+
+  /**
+   * Подписка слушателя на изменения состояния
+   * @param listener {Function}
+   * @returns {Function} Функция отписки
+   */
+  subscribe(listener) {
+    this.listeners.push(listener);
+
+    // Возвращается функция для удаления добавленного слушателя
+    return () => {
+      this.listeners = this.listeners.filter(item => item !== listener);
+    };
+  }
+
+  /**
+   * Выбор состояния
+   * @returns {Object}
+   */
+  getState() {
+    return this.state;
+  }
+
+  /**
+   * Установка состояния
+   * @param newState {Object}
+   */
+  setState(newState) {
+    this.state = newState;
+
+    // Вызываем всех слушателей
+    for (const listener of this.listeners) listener();
+  }
+
+  /**
+   * Добавление новой записи
+   */
+  addItem() {
+    this.setState({
+      ...this.state,
+      list: [...this.state.list, { code: this.state.listLength, title: 'Новая запись' }],
+      listLength: this.state.listLength + 1,
+    });
+    // console.log(Store.getState().listLength);
+    console.log(this.state.listLength);
+  }
+
+  /**
+   * Удаление записи по коду
+   * @param code
+   */
+  deleteItem(code) {
+    this.setState({
+      ...this.state,
+      list: this.state.list.filter(item => item.code !== code),
+    });
+  }
+
+  /**
+   * Выделение записи по коду
+   * @param code
+   */
+  selectItem(code) {
+    this.setState({
+      ...this.state,
+      list: this.state.list.map(item => {
+        if (item.code === code) {
+          if (item.selected === false) {
+            item = { ...item, selection: item.selection + 1 };
+          }
+          item.selected = !item.selected;
+        } else {
+          item.selected = false;
+        }
+        return item;
+      }),
+    });
+  }
+}
+
+export default Store;
